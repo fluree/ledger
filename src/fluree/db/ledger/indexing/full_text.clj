49bdfcfe38565a69a2c7c52a8258e111101c7fd7
ben lamothe <bl@732.one>
2021-01-14 15:55:54 -0500

    make predicate maps instead of lists of pairs

--- src/fluree/db/ledger/indexing/full_text.clj
+++ src/fluree/db/ledger/indexing/full_text.clj
@@ -26,12 +26,12 @@
 
   Each item in the output channel has the following structure:
 
-  [`s` [[`p` `o`] ...]]
+  [`s` {`p1` `o1`, `p2` `o2`, ...}]
 
   where the first element `s` is a subject id that appears at most once in the
-  output stream, and the second element is a list of pairs where each pair `p`
-  and `o` is the predicate and object, respectfully, from one of the input
-  flakes for the subject `s`."
+  output stream, and the second element is a map where each key-value pair is
+  the predicate and object from a flake appearing in the input stream for that
+  subject."
   [flake-chan]
   (let [;; reduce the flakes in `flake-chan` into a map whose keys are subject
         ;; ids and values are lists of all the predicate-object pairs appearing
@@ -40,11 +40,11 @@
                                        (let [subj (.-s f)
                                              pred (.-p f)
                                              obj  (.-o f)]
-                                         (update m subj conj [pred obj])))
+                                         (update m subj assoc pred obj)))
                                      {} flake-chan)
 
-        ;; turn a stream of maps into a stream of key-value pairs that appear in
-        ;; each map
+        ;; turn a stream of maps into a stream of the key-value pairs that
+        ;; appear in each map
         out-chan       (chan nil (mapcat seq))]
 
     (async/pipe subj-attr-chan out-chan)
