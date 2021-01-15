0883af0bd46958542165a393945ecb7ba7476e12
ben lamothe <bl@732.one>
2021-01-15 14:08:06 -0500

    fns to index and purge a flake stream

--- src/fluree/db/ledger/indexing/full_text.clj
+++ src/fluree/db/ledger/indexing/full_text.clj
@@ -1,8 +1,8 @@
 (ns fluree.db.ledger.indexing.full-text
-  (:require [clojure.core.async :as async :refer [<! >! chan go go-loop]]
+  (:require [fluree.db.full-text :as full-text]
+            [clojure.core.async :as async :refer [<! >! chan go go-loop]]
             [clojure.string :as str]
             [clojure.tools.logging :as log]
-            [fluree.db.full-text :as full-text]
             [clucie.core :as lucene]
             [clucie.store :as lucene-store])
   (:import fluree.db.flake.Flake))
@@ -21,32 +21,24 @@
   the predicate and object from a flake appearing in the input stream for that
   subject."
   [flake-chan]
-  (let [;; reduce the flakes in `flake-chan` into a map whose keys are subject
-        ;; ids and values are lists of all the predicate-object pairs appearing
-        ;; in the `flake-chan` for that subject.
-        subj-attr-chan (async/reduce (fn [m ^Flake f]
-                                       (let [subj (.-s f)
-                                             pred (.-p f)
-                                             obj  (.-o f)]
-                                         (update m subj assoc pred obj)))
-                                     {} flake-chan)
-
-        ;; turn a stream of maps into a stream of the key-value pairs that
-        ;; appear in each map
-        out-chan       (chan nil (mapcat seq))]
-
-    (async/pipe subj-attr-chan out-chan)
-
-    out-chan))
+  (let [subj-map-chan (async/reduce (fn [m ^Flake f]
+                                      (let [subj (.-s f)
+                                            pred (.-p f)
+                                            obj  (.-o f)]
+                                        (update m subj assoc pred obj)))
+                                    {} flake-chan)
+        out-chan      (chan nil (mapcat seq))]
+    (async/pipe subj-map-chan out-chan)))
 
 (defn process-subjects
   "Processes each subject - predicate-map pair in `subj-chan` with the supplied
-  `processor` function. The processor should modify the index with the writer
-  and return updated stats tracking successes and errors"
-  [processor writer init-stats subj-chan]
+  `processor` function. The processor should modify the index for each item in
+  the `subj-chan` and return updated stats tracking successes and errors.
+  Returns a channel that will eventually contain the updated stats."
+  [processor init-stats subj-chan]
   (go-loop [stats init-stats]
     (if-let [[subj pred-map] (<! subj-chan)]
-      (let [stats* (processor writer stats subj pred-map)]
+      (let [stats* (processor stats subj pred-map)]
         (recur stats*))
       stats)))
 
@@ -54,7 +46,7 @@
   "Add the subjects from `subj-chan` to the index using the `writer`. Keeps track
   of successfully `:indexed` subjects and `:errors` in the stats map returned."
   [writer init-stats subj-chan]
-  (process-subjects (fn [writer stats subj pred-map]
+  (process-subjects (fn [stats subj pred-map]
                       (try
                         (full-text/put-subject writer subj pred-map)
                         (log/trace "Indexed full text predicates for subject "
@@ -63,18 +55,23 @@
 
                         (catch Exception e
                           (log/error e (str "Error indexing full text "
-                                            "predicates for subject "
-                                            subj))
+                                            "predicates for subject " subj))
                           (update stats :errors inc))))
 
-                    writer init-stats subj-chan))
+                    init-stats subj-chan))
+
+(defn index-flakes
+  [writer init-stats flake-chan]
+  (->> flake-chan
+       group-by-subject
+       (index-subjects writer init-stats)))
 
 (defn purge-subjects
   "Remove the included predicates for each subject from `subj-chan` from the index
   using the `writer`. Keeps track of successfully `:purged` subjects and
   `:errors` in the stats map returned."
   [init-stats writer subj-chan]
-  (process-subjects (fn [writer stats subj pred-map]
+  (process-subjects (fn [stats subj pred-map]
                       (try
                         (full-text/purge-subject)
                         (log/trace "Purged stale full text predicates for "
@@ -86,5 +83,11 @@
                                             "predicates for subject " subj))
                           (update stats :errors inc))))
 
-                    writer init-stats subj-chan))
+                    init-stats subj-chan))
+
+(defn purge-flakes
+  [writer init-stats flake-chan]
+  (->> flake-chan
+       group-by-subject
+       (purge-subjects writer init-stats)))
 
