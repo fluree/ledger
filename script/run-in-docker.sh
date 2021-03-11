1d3a3118b1d460bedb25c57d89d49370fb4599d2
Wes Morgan <wesmorgan@hey.com>
2021-03-11 10:18:32 -0700

    Build release in docker builder image in CD

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -9,4 +9,4 @@ builder_image=${image}:builder
 echo "Running in ${builder_image} container..."
 
 docker build --quiet --target builder --tag "${builder_image}" .
-docker run --rm "${builder_image}" "$@"
+docker run --rm ${DOCKER_RUN_ARGS} "${builder_image}" "$@"
