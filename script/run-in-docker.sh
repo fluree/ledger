86d65c66710aab50bbf0af0980722595eaa4a62f
Wes Morgan <wesmorgan@hey.com>
2021-03-15 11:05:58 -0600

    Make run-in-docker.sh even quieter

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -6,5 +6,8 @@ image=fluree/${PWD##*/}
 
 builder_image=${image}:builder
 
-docker build --quiet --target builder --tag "${builder_image}" .
+# output build stdout to /dev/null b/c even with --quiet it still outputs
+# sha256:blahblahblah and we sometimes want to consume the docker run output
+# in another script
+docker build --quiet --target builder --tag "${builder_image}" . >/dev/null
 docker run --rm ${DOCKER_RUN_ARGS} "${builder_image}" "$@"
