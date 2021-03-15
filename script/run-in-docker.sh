4b0cfa1a90302534695fbcb4f472c41dab40477a
Wes Morgan <wesmorgan@hey.com>
2021-03-15 09:55:52 -0600

    Stop outputting "Running in blah" in run-in-docker.sh
    ...in case someone wants to capture the container's output for something
    else in an automated process (e.g. CI/CD).

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -6,7 +6,5 @@ image=fluree/${PWD##*/}
 
 builder_image=${image}:builder
 
-echo "Running in ${builder_image} container..."
-
 docker build --quiet --target builder --tag "${builder_image}" .
 docker run --rm ${DOCKER_RUN_ARGS} "${builder_image}" "$@"
