d702a49c9b89487a26b9d8e26dd3426acd6390e3
Brian Platz <bplatz@gmail.com>
2021-04-01 17:29:40 -0400

    Break out auth resolution logic to make more clear

--- src/fluree/db/ledger/transact/auth.clj
+++ src/fluree/db/ledger/transact/auth.clj
@@ -5,39 +5,67 @@
             [fluree.db.dbproto :as dbproto]
             [fluree.db.util.core :as util]
             [fluree.db.auth :as auth]
-            [fluree.db.permissions :as permissions]))
+            [fluree.db.permissions :as permissions]
+            [fluree.db.util.log :as log]))
 
 (defn- valid-authority?
   [db auth authority]
-  (go-try
-    (if (empty? (<? (dbproto/-search db [auth "_auth/authority" authority])))
-      (throw (ex-info (str authority " is not an authority for auth: " auth)
-                      {:status 403 :error :db/invalid-auth})) true)))
+  (async/go
+    (let [connection? (async/<! (dbproto/-search db [auth "_auth/authority" authority]))]
+      (cond (util/exception? connection?)
+            (do (log/error connection? "valid-authority? search for connection between " [auth "_auth/authority" authority]
+                           "unexpectedly failed!")
+                false)
+
+            (empty? connection?)
+            false
+
+            :else
+            true))))
+
+
+(defn- resolve-auth+authority-sids
+  "Returns two-tuple of [auth-sid and authority-sid].
+  If no authority exists, returns nil for authority-sid.
+
+  Performs lookups in parallel.
+
+  Returns exception if both auth and authority (when applicable)
+  do not resolve to an _auth/id"
+  [db auth authority]
+  (async/go
+    (let [auth-id-ch    (dbproto/-subid db ["_auth/id" auth] true)
+          ;; kick off authority check in parallel (when applicable)
+          authority-sid (when authority
+                          (let [authority-id (if (string? authority) ["_auth/id" authority] authority)]
+                            (async/<! (dbproto/-subid db authority-id true))))
+          auth-sid      (async/<! auth-id-ch)]
+      (cond
+        (util/exception? auth-sid)
+        (ex-info (str "Auth id for transaction does not exist in the database: " auth)
+                 {:status 403 :error :db/invalid-auth})
+
+        (util/exception? authority-sid)
+        (ex-info (str "Authority " authority " does not exist.")
+                 {:status 403 :error :db/invalid-auth})
+
+        (and authority-sid (false? (async/<! (valid-authority? db auth-sid authority-sid))))
+        (ex-info (str authority " is not an authority for auth: " auth)
+                 {:status 403 :error :db/invalid-auth})
+
+        :else
+        [auth-sid authority-sid]))))
+
 
 (defn add-auth-ids-permissions
   "Figures out transaction permissions, returns map."
   [db tx-map]
   (go-try
     (let [{:keys [auth authority]} tx-map
-          auth-id-ch      (dbproto/-subid db ["_auth/id" auth] true)
-          authority-id-ch (when authority
-                            (let [authority-id (if (string? authority) ["_auth/id" authority] authority)]
-                              (dbproto/-subid db authority-id true)))
-          auth-sid        (async/<! auth-id-ch)
-          _               (when (util/exception? auth-sid)
-                            (throw (ex-info (str "Auth id for transaction does not exist in the database: " auth)
-                                            {:status 403 :error :db/invalid-auth})))
-          ;; validate authority is valid or throw
-          authority-sid   (when authority
-                            (let [authority_id (async/<! authority-id-ch)
-                                  _            (when (util/exception? authority_id)
-                                                 (throw (ex-info (str "Authority " authority " does not exist.")
-                                                                 {:status 403 :error :db/invalid-auth})))]
-                              (<? (valid-authority? db auth-sid authority_id))
-                              authority_id))
-          roles           (<? (auth/roles db auth-sid))
-          tx-permissions  (-> (<? (permissions/permission-map db roles :transact))
-                              (assoc :auth auth-sid))]
-      (assoc tx-map :auth-sid       auth-sid
-                    :authority-sid  authority-sid
+          [auth-sid authority-sid] (<? (resolve-auth+authority-sids db auth authority))
+          roles          (<? (auth/roles db auth-sid))
+          tx-permissions (-> (<? (permissions/permission-map db roles :transact))
+                             (assoc :auth auth-sid))]
+      (assoc tx-map :auth-sid auth-sid
+                    :authority-sid authority-sid
                     :tx-permissions tx-permissions))))
