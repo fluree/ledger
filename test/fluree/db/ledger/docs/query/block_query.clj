ce072b42f5c5e6cb4726b5100acaf0004d06ee74
ben lamothe <bl@732.one>
2021-04-08 21:02:09 -0400

    fix block-query-test testing macros

--- test/fluree/db/ledger/docs/query/block_query.clj
+++ test/fluree/db/ledger/docs/query/block_query.clj
@@ -9,83 +9,88 @@
 (use-fixtures :once test/test-system)
 
 (deftest query-single-block
-  (testing "Select single block by block number")
-  (let [query {:block 3}
-        res   (-> (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query)) first)]
+  (testing "Select single block by block number"
+    (let [query {:block 3}
+          res   (-> (basic/get-conn)
+                    (fdb/block-query-async test/ledger-chat query)
+                    async/<!!
+                    first)]
 
-    ;; TODO: Account for block metadata missing from the database `block`
-    ;; subject flakes for the transaction adding each block including
-    ;; `:block-bytes` and `:cmd-types`
-    (is (= (-> res keys set) #{:block :hash :instant :txns :t :sigs :flakes}))
+      ;; TODO: Account for block metadata missing from the database `block`
+      ;; subject flakes for the transaction adding each block including
+      ;; `:block-bytes` and `:cmd-types`
+      (is (= (-> res keys set) #{:block :hash :instant :txns :t :sigs :flakes}))
 
-    ;; TODO: Account for filtered meta predicates which appear in block files
-    ;;       but not indexes. see fluree.db.graphdb/exclude-predicates
-    ;; (is (= 82 (count (:flakes res))))
-    (is (= 79 (count (:flakes res))))))
+      ;; TODO: Account for filtered meta predicates which appear in block files
+      ;;       but not indexes. see fluree.db.graphdb/exclude-predicates
+      ;; (is (= 82 (count (:flakes res))))
+      (is (= 79 (count (:flakes res)))))))
 
 (deftest query-single-block-with-ISO-string
-  (testing "Select single block with ISO-8601 wall clock time")
-  (let [query {:block "2017-11-14T20:59:36.097Z"}
-        res   (try (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))
-                   (catch Exception e e))]
-    (is (= "There is no data as of 1510693176097" (.getMessage res)))))
+  (testing "Select single block with ISO-8601 wall clock time"
+    (let [query {:block "2017-11-14T20:59:36.097Z"}
+          res   (try (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))
+                     (catch Exception e e))]
+      (is (= "There is no data as of 1510693176097" (.getMessage res))))))
 
 
 
 (deftest query-single-block-with-duration
-  (testing "Select single block with duration string")
-  (let [query {:block "PT1H"}
-        res   (try (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))
-                   (catch Exception e e))]
-    (is (str/includes? (.getMessage res) "There is no data as of "))))
+  (testing "Select single block with duration string"
+    (let [query {:block "PT1H"}
+          res   (try (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))
+                     (catch Exception e e))]
+      (is (str/includes? (.getMessage res) "There is no data as of ")))))
 
 
 ;; TODO - looks like block range now inclusive? Fix.
 (deftest query-block-range
-  (testing "Select ranges of blocks")
-  (let [query {:block [3 5]}
-        res    (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))]
-    (is (= 3 (count res)))
+  (testing "Select ranges of blocks"
+    (let [query {:block [3 5]}
+          res   (-> (basic/get-conn)
+                    (fdb/block-query-async test/ledger-chat query)
+                    async/<!!)]
+      (is (= 3 (count res)))
 
-    (is (= (-> res first keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
+      (is (= (-> res first keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
 
-    (is (= (-> res second keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
+      (is (= (-> res second keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
 
-    (is (nil? (nth res 2)))))
+      (is (nil? (nth res 2))))))
 
 
 (deftest query-block-range-lower-limit
-  (testing "Select ranges of blocks")
-  (let [query {:block [3]}
-        res   (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))]
+  (testing "Select ranges of blocks"
+    (let [query {:block [3]}
+          res   (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))]
 
-    (is (= 2 (count res)))
+      (is (= 2 (count res)))
 
-    (is (= (-> res first keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
+      (is (= (-> res first keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
 
-    (is (= (-> res second keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))))
+      (is (= (-> res second keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes})))))
 
 
 (deftest query-block-range-pretty-print
-  (testing "Pretty prints a block range")
-  (let [query {:block [3]
-               :prettyPrint true}
-        res    (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))
-        flakeKeys3 (-> res second :flakes keys set)]
+  (testing "Pretty prints a block range"
+    (let [query {:block [3]
+                 :prettyPrint true}
+          res    (async/<!! (fdb/block-query-async (basic/get-conn) test/ledger-chat query))
+          flakeKeys3 (-> res second :flakes keys set)]
 
-    (is (= 2 (count res)))
+      (is (= 2 (count res)))
 
-    (is (= (-> res first keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
+      (is (= (-> res first keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
 
-    (is (= (-> res second keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
+      (is (= (-> res second keys set) #{:block :hash :instant :txns :block-bytes :cmd-types :t :sigs :flakes}))
 
-    (is (= flakeKeys3 #{:asserted :retracted}))))
+      (is (= flakeKeys3 #{:asserted :retracted})))))
 
 (deftest block-query-test
   (query-single-block)
   (query-single-block-with-ISO-string)
   (query-single-block-with-duration))
-  ;(query-block-range)
+  ;; (query-block-range)
   ;(query-block-range-lower-limit)
   ;(query-block-range-pretty-print)
 
