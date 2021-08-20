7b2030d1006c0384210ea7bed3887816c63d735e
Lois Deak-Williams <lois@flur.ee>
2021-08-20 08:48:16 -0400

    use some-> in case prev-hash is not found

--- src/fluree/db/ledger/transact.clj
+++ src/fluree/db/ledger/transact.clj
@@ -38,10 +38,10 @@
           block         (inc (:block db-current))
           block-instant (util/current-time-millis)
           before-t      (:t db-current)
-          prev-hash     (-> (query-range/index-range db-current :spot = [before-t const/$_block:hash])
-                            <?
-                            first
-                            .-o) ; get hash (object) from flake
+          prev-hash     (some-> (query-range/index-range db-current :spot = [before-t const/$_block:hash])
+                                <?
+                                first
+                                .-o) ; get hash (object) from flake
           _             (when-not prev-hash
                           (throw (ex-info (str "Unable to retrieve previous block hash. Unexpected error.")
                                           {:status 500
