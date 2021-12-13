77c19ac792026391286f974d6f357e345f3258ee
Wes Morgan <git@wesmorgan.me>
2021-12-13 16:26:25 -0700

    Try enabling Docker BuildKit in CI

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -6,8 +6,10 @@ image=fluree/${PWD##*/}
 
 builder_image=${image}:builder
 
+export DOCKER_BUILDKIT=1
+
 # output build stdout to /dev/null b/c even with --quiet it still outputs
 # sha256:blahblahblah and we sometimes want to consume the docker run output
 # in another script
 docker build --quiet --target builder --tag "${builder_image}" . >/dev/null
-docker run --rm ${DOCKER_RUN_ARGS} "${builder_image}" "$@"
+docker run --rm "${DOCKER_RUN_ARGS}" "${builder_image}" "$@"
