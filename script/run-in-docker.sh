bd3b8eaa89cec6defbba56003fecd4bfd9fa0d03
Wes Morgan <git@wesmorgan.me>
2021-12-13 16:34:30 -0700

    Try another thing to fix run-in-docker.sh

--- script/run-in-docker.sh
+++ script/run-in-docker.sh
@@ -11,5 +11,5 @@ export DOCKER_BUILDKIT=1
 # output build stdout to /dev/null b/c even with --quiet it still outputs
 # sha256:blahblahblah and we sometimes want to consume the docker run output
 # in another script
-docker build --quiet --target builder --tag "${builder_image}" --load . >/dev/null
-docker run --rm "${DOCKER_RUN_ARGS}" "${builder_image}" "$@"
+docker build --quiet --target builder --tag "${builder_image}" . >/dev/null
+docker run --rm ${DOCKER_RUN_ARGS} "${builder_image}" "$@"
