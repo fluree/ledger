c93c0459a2863b47dc46d8dda85a66027f2c1043
Wes Morgan <git@wesmorgan.me>
2022-01-25 09:11:59 -0700

    Address PR feedback in signatures-test ns

--- test/fluree/db/ledger/docs/identity/signatures_test.clj
+++ test/fluree/db/ledger/docs/identity/signatures_test.clj
@@ -31,8 +31,10 @@
       json/parse))
 
 
-;; Create auth records -> root, all persons, all persons no handles
-(defn create-auths [ledger]
+(defn create-auths
+  "Creates 3 auths in the given ledger: root, all persons, all persons no
+  handles. Returns of vector of [key-maps create-txn-result]."
+  [ledger]
   (let [keys     (vec (repeatedly 3 fdb/new-private-key))
         add-auth [{:_id   "_auth"
                    :id    (get-in keys [0 :id])
@@ -74,8 +76,6 @@
     (is (= 28 (count flakes)))))
 
 
-;; Endpoint test - unsigned queries should throw an error, no signature
-
 (deftest unsigned-queries-test
   (testing "An unsigned query should throw an error"
     (let [ledger       (test/rand-ledger test/ledger-chat)
@@ -95,79 +95,76 @@
       (is (every? #(= "Request requires authentication." (:message %)) resps)))))
 
 
-;; Endpoint test - /query - should return filtered results
-
 (deftest signed-queries-test
-  (let [ledger           (test/rand-ledger test/ledger-chat)
+  (testing "/query endpoint should return filtered results"
+    (let [ledger           (test/rand-ledger test/ledger-chat)
 
-        [keys] (create-auths ledger)
+          [keys] (create-auths ledger)
 
-        _                (test/transact-schema ledger "chat.edn" :clj)
-        _                (test/transact-schema ledger "chat-preds.edn" :clj)
-        _                (test/transact-data ledger "chat.edn" :clj)
+          _                (test/transact-schema ledger "chat.edn" :clj)
+          _                (test/transact-schema ledger "chat-preds.edn" :clj)
+          _                (test/transact-data ledger "chat.edn" :clj)
 
-        req              {:headers {"content-type" "application/json"}
-                          :body    (json/stringify {:select ["*"] :from "person"})}
+          req              {:headers {"content-type" "application/json"}
+                            :body    (json/stringify {:select ["*"] :from "person"})}
 
-        q-endpoint       (str endpoint ledger "/query")
+          q-endpoint       (str endpoint ledger "/query")
 
-        root-auth        (get-in keys [0 :private])
-        person-auth      (get-in keys [1 :private])
-        no-handle-auth   (get-in keys [2 :private])
+          root-auth        (get-in keys [0 :private])
+          person-auth      (get-in keys [1 :private])
+          no-handle-auth   (get-in keys [2 :private])
 
-        signed-root      (http-signatures/sign-request :post q-endpoint req
-                                                       root-auth)
-        signed-person    (http-signatures/sign-request :post q-endpoint req
-                                                       person-auth)
-        signed-no-handle (http-signatures/sign-request :post q-endpoint req
-                                                       no-handle-auth)
+          signed-root      (http-signatures/sign-request :post q-endpoint req
+                                                         root-auth)
+          signed-person    (http-signatures/sign-request :post q-endpoint req
+                                                         person-auth)
+          signed-no-handle (http-signatures/sign-request :post q-endpoint req
+                                                         no-handle-auth)
 
-        {root-body :body :as root-resp}
-        (-> signed-root
-            (->> (http/post q-endpoint))
-            deref
-            (test/safe-update :body string? json/parse))
+          {root-body :body :as root-resp}
+          (-> signed-root
+              (->> (http/post q-endpoint))
+              deref
+              (test/safe-update :body string? json/parse))
 
-        {person-body :body :as person-resp}
-        (-> signed-person
-            (->> (http/post q-endpoint))
-            deref
-            (test/safe-update :body string? json/parse))
+          {person-body :body :as person-resp}
+          (-> signed-person
+              (->> (http/post q-endpoint))
+              deref
+              (test/safe-update :body string? json/parse))
 
-        {no-handle-body :body :as no-handle-resp}
-        (-> signed-no-handle
-            (->> (http/post q-endpoint))
-            deref
-            (test/safe-update :body string? json/parse))
+          {no-handle-body :body :as no-handle-resp}
+          (-> signed-no-handle
+              (->> (http/post q-endpoint))
+              deref
+              (test/safe-update :body string? json/parse))
 
-        responses        [root-resp person-resp no-handle-resp]
-        bodies           [root-body person-body no-handle-body]]
+          responses        [root-resp person-resp no-handle-resp]
+          bodies           [root-body person-body no-handle-body]]
 
-    (is (every? #(= 200 %) (map :status responses)))
+      (is (every? #(= 200 %) (map :status responses)))
 
-    (is (apply = (map count bodies)))
+      (is (apply = (map count bodies)))
 
-    (is (some :person/handle root-body))
+      (is (some :person/handle root-body))
 
-    (is (some :person/handle person-body))
+      (is (some :person/handle person-body))
 
-    (is (not-any? :person/handle no-handle-body))))
+      (is (not-any? :person/handle no-handle-body)))))
 
-
-;; Endpoint test - /transact - should throw error, /command endpoint shouldn't
 
 (deftest transact-endpoint-test
-  (testing "The /transact endpoint should not work.")
-  (let [ledger     (test/rand-ledger test/ledger-chat)
-        add-person [{:_id "person" :handle "anna"}]
-        {:keys [status]} (->> add-person
-                              test/standard-request
-                              (http/post (str endpoint ledger "/transact"))
-                              deref
-                              :body
-                              json/parse)]
+  (testing "The /transact endpoint should not work."
+    (let [ledger     (test/rand-ledger test/ledger-chat)
+          add-person [{:_id "person" :handle "anna"}]
+          {:keys [status]} (->> add-person
+                                test/standard-request
+                                (http/post (str endpoint ledger "/transact"))
+                                deref
+                                :body
+                                json/parse)]
 
-    (is (= 401 status))))
+      (is (= 401 status)))))
 
 
 (defn sign-cmd-fetch-tx-id
@@ -179,26 +176,25 @@
                                 {:select ["*"] :from ["_tx/id" tx-id]}))))
 
 
-;; Endpoint test - /command - should allow txns based on auth permissions
-
 (deftest signed-commands
-  (let [ledger        (test/rand-ledger test/ledger-chat)
-        _             (test/transact-schema ledger "chat.edn" :clj)
-        _             (test/transact-schema ledger "chat-preds.edn" :clj)
+  (testing "/command endpoint should (dis)allow txns based on auth permissions"
+    (let [ledger        (test/rand-ledger test/ledger-chat)
+          _             (test/transact-schema ledger "chat.edn" :clj)
+          _             (test/transact-schema ledger "chat-preds.edn" :clj)
 
-        [keys] (create-auths ledger)
+          [keys] (create-auths ledger)
 
-        root-key      (get-in keys [0 :private])
-        person-key    (get-in keys [1 :private])
-        no-handle-key (get-in keys [2 :private])
+          root-key      (get-in keys [0 :private])
+          person-key    (get-in keys [1 :private])
+          no-handle-key (get-in keys [2 :private])
 
-        opts          {:throw-exceptions false}
-        txn           [{:_id "chat" :message "hi!"}]
-        resps         (mapcat #(sign-cmd-fetch-tx-id ledger % txn opts)
-                              [root-key person-key no-handle-key])
-        errs          (map #(get % "_tx/error") resps)]
+          opts          {:throw-exceptions false}
+          txn           [{:_id "chat" :message "hi!"}]
+          resps         (mapcat #(sign-cmd-fetch-tx-id ledger % txn opts)
+                                [root-key person-key no-handle-key])
+          errs          (map #(get % "_tx/error") resps)]
 
-    (is (nil? (first errs)))
+      (is (nil? (first errs)))
 
-    (is (= "400 db/write-permission Insufficient permissions for predicate: chat/message within collection: chat."
-           (second errs) (nth errs 2)))))
+      (is (= "400 db/write-permission Insufficient permissions for predicate: chat/message within collection: chat."
+             (second errs) (nth errs 2))))))
