0259745587685a759c1a7eef6c0cd4bc7428e389
Lois Deak-Williams <lois@flur.ee>
2022-02-17 11:03:36 -0500

    FC-1336, FC-1339, FC-1340 basically reworked purge into smaller tasks

--- src/fluree/db/ledger/mutable.clj
+++ src/fluree/db/ledger/mutable.clj
@@ -6,7 +6,9 @@
             [fluree.db.storage.core :as storage]
             [fluree.db.constants :as const]
             [fluree.db.util.log :as log]
-            [fluree.db.ledger.reindex :as reindex])
+            [fluree.db.ledger.reindex :as reindex]
+            [fluree.db.util.core :as util]
+            [fluree.db.flake :as flake])
   (:import (fluree.db.flake Flake)))
 
 (set! *warn-on-reflection* true)
@@ -58,15 +60,38 @@
             (log/warn (str "Flakes purged from block " block))
             true)))
 
+
+(defn- create-block-map
+  "Creates a block-map from a vector of flakes"
+  [db flakes]
+  (go-try
+    (loop [[^Flake flake & r] flakes
+           t-map     {}
+           block-map {}]
+      (if flake
+        (if-let [block (get t-map (.-t flake))]
+          (recur r t-map (update block-map block conj flake))
+          (let [t     (.-t flake)
+                block (<? (time-travel/non-border-t-to-block db t))]
+            (if (get block-map block)
+              (recur r (assoc t-map t block) (update block-map block conj flake))
+              (recur r (assoc t-map t block) (assoc block-map block [flake])))))
+        block-map))))
+
+
 (defn identify-hide-blocks-flakes
   [db {:keys [block hide purge] :as query-map}]
   (go-try (let [[pattern idx] (fdb/get-history-pattern (or hide purge))
-                [block-start block-end] (when block (<? (fdb/resolve-block-range db query-map)))
+                [block-start block-end]
+                  (when block (<? (fdb/resolve-block-range db query-map)))
                 from-t    (if (and block-start (not= 1 block-start))
-                            (dec (:t (<? (time-travel/as-of-block db (dec block-start))))) -1)
+                            (dec (:t (<? (time-travel/as-of-block db (dec block-start)))))
+                            -1)
                 to-t      (if block-end
-                            (:t (<? (time-travel/as-of-block db block-end))) (:t db))
+                            (:t (<? (time-travel/as-of-block db block-end)))
+                            (:t db))
                 flakes    (<? (query-range/time-range db idx = pattern {:from-t from-t :to-t to-t}))
+                _ (log/info "identify-hide-blocks-flakes " {:flakes flakes})
                 block-map (<? (go-try (loop [[^Flake flake & r] flakes
                                              t-map     {}
                                              block-map {}]
@@ -102,25 +127,125 @@
                       :blocks        (keys block-map)}
              :fuel   fuel})))
 
-(defn purge-flakes
-  [conn nw ledger query-map]
+(defn in?
+  "true if coll contains elm"
+  [coll elm]
+  (some #(= elm %) coll))
+
+(defn validate-purge-params
+  "Validate provided subject id exists"
+  [param]
+  (cond
+    (util/subj-ident? param)
+    [param]
+
+    (sequential? param)
+    (if-not (seq param)
+      (throw (ex-info (str "Please specify a valid subject. Provided: " param)
+                      {:status 400
+                       :error  :db/invalid-ident}))
+      param)
+
+    :else
+    (throw (ex-info (str "Invalid subject. Provided: " param)
+                    {:status 400
+                     :error  :db/invalid-ident}))))
+
+(defn get-component-preds-from-schema
+  "Return ids of predicates designated as a component"
+  [db]
+  (->> db
+       :schema
+       :pred
+       (reduce-kv
+         (fn [m k v]
+           (if (and (number? k) (:component v))
+             (into m [k])
+             m))
+         [])
+       set))
+
+(defn identify-component-sids
+  "Given a set of flakes, traverse schema to follow components"
+  [preds flakes]
+  (->> flakes
+       (filter #(some preds %))
+       (reduce (fn [m ^Flake v]
+                 (conj  m (.-o v)))
+               [])))
+
+(defn identify-purge-graph
+  "Return a vector of vectors
+   * 1st element - a vector of flakes where provided ID is either the subject or object reference
+   * 2nd element - a vector of sids for component references (similar to a cascade delete)"
+  [db preds sid]
+  (go-try
+    (let [s-flakes  (->> (flake/parts->Flake [sid nil nil nil])
+                         (query-range/search db)
+                         <?)
+          o-flakes  (->> (flake/parts->Flake [nil nil sid nil])
+                         (query-range/search db)
+                         <?)
+          comp-sids (identify-component-sids preds s-flakes)]
+      [(concat s-flakes o-flakes) comp-sids])))
+
+(defn identify-purge-flakes
+  "Return a vector of flakes to be purged based on the provided sids"
+  [db preds sids]
+  (go-try
+    (loop [[sid & r] sids ;; sids to be processed; this list can grow if components are found
+           sid-list sids  ;; complete list of sids targeted for purge
+           flakes []]     ;; vector of flakes to be purged; can be duplicates
+      (if sid
+        (let [[so-flakes comp-sids] (<? (identify-purge-graph db preds sid))
+              new-sids              (filter #(not (in? sid-list %)) comp-sids)]
+          (recur (concat r new-sids)         ;; add any new sids to the "to be processed" vector
+                 (concat sid-list new-sids)  ;; add any new sids to the vector of targeted sids
+                 (concat flakes so-flakes))) ;; add flakes to purge vector
+        (into [] (distinct) flakes)          ;; get rid of duplicate flakes
+        ))))
+
+(defn identify-purge-map
+  "Generate the map of blocks->flakes to be purged from the ledger"
+  [db preds sids]
+  (go-try
+    (let [flakes    (<? (identify-purge-flakes db preds sids))
+          block-map (<? (create-block-map db flakes))]
+      [block-map (count flakes)])))
+
   ;; TODO - this can take some time. Need a good way to handle this.
-  (go-try (let [db         (<? (fdb/db conn (str nw "/" ledger)))
-                ;; TODO - this doesn't work if you've previously hidden the data.
-                [block-map fuel] (<? (identify-hide-blocks-flakes db query-map))
-                _          (when (not-empty block-map)
+(defn purge-flakes
+  "Purge all flakes referencing a subject.
+  Currently, only a single subject can be processed at a time"
+  [conn nw ledger {:keys [purge]}]
+  (go-try
+    (let [sids             (validate-purge-params purge)
+          db               (<? (fdb/db conn (str nw "/" ledger)))
+          preds            (get-component-preds-from-schema db)
+
+          ;; TODO - this doesn't work if you've previously hidden the data.
+          [block-map fuel] (<? (identify-purge-map db preds sids))
+          _                (when (seq block-map)
                              (loop [[[block flakes] & r] block-map]
                                (<?? (purge-block conn nw ledger block flakes))
-                               (if r (recur r)
-                                     true)))
-                ;; Pass in a custom ecount, so as not to have multiple items
-                ;; with same subject id
-                old-ecount (:ecount db)
-                _          (<? (reindex/reindex conn nw ledger {:ecount old-ecount}))]
-            {:status 200
-             :result {:flakes-purged fuel
-                      :blocks        (keys block-map)}
-             :fuel   fuel})))
+                               (if r
+                                 (recur r)
+                                 true)))
+          ;; Pass in a custom ecount, so as not to have multiple items
+          ;; with same subject id
+          old-ecount (:ecount db)
+          _          (<? (reindex/reindex conn nw ledger {:ecount old-ecount}))
+          ;; Kick-off process to re-index full-text
+          ;; TODO - consider amount of fuel for full-text indexing
+          indexer    (-> conn :full-text/indexer :process)
+          ft-res     (if indexer
+                       (<? (indexer {:action :reset, :db db}))
+                       :none)]
+      {:status 200
+       :result {:flakes-purged fuel
+                :blocks        (keys block-map)
+                :full-text     ft-res}
+       :fuel   fuel})))
 
 (comment
   (require '[fluree.db.serde.protocol :as serdeproto])
