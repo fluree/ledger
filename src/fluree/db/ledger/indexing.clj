731e2585161c931e5e20aaa4fb20277167709e22
Benjamin Lamothe <bl@732.one>
2022-02-22 09:48:20 -0500

    pass parallelism in to tree-chan

--- src/fluree/db/ledger/indexing.clj
+++ src/fluree/db/ledger/indexing.clj
@@ -248,7 +248,7 @@
         novel?     (fn [node]
                      (or (seq remove-preds)
                          (seq (index/novelty-subrange node t novelty))))]
-    (->> (index/tree-chan conn root novel? (constantly true) refresh-xf error-ch)
+    (->> (index/tree-chan conn root novel? (constantly true) 1 refresh-xf error-ch)
          (write-resolved-nodes conn network dbid idx error-ch))))
 
 (defn extract-root
