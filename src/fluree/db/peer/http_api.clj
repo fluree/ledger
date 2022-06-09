cc887a625de5a39d6ab9a50fb22e76958ba7038e
Daniel Petranek <dpetranek@flur.ee>
2022-06-09 16:59:39 -0500

    Use ledger-id instead of dbid
    This matches the terminology we are using in fluree.db.storage.core
    
    The TODO is for after corresponding changes to the db library published.

--- src/fluree/db/peer/http_api.clj
+++ src/fluree/db/peer/http_api.clj
@@ -779,10 +779,10 @@
                                            (throw (ex-info (str "If using a closed api, a storage request must be returned as json.")
                                                            {:status 401
                                                             :error  :db/invalid-transaction})))
-                        {network :network dbid :ledger type :type key :key} params
-                        ledger        (keyword network ledger)
-                        _                (when-not (and network dbid type)
-                                           (throw (ex-info (str "Incomplete request. At least a network, db and type are required. Provided network: " network " ledger: " dbid " type: " type " key: " key)
+                        {network :network ledger-id :ledger type :type key :key} params
+                        ledger        (keyword network ledger-id)
+                        _                (when-not (and network ledger-id type)
+                                           (throw (ex-info (str "Incomplete request. At least a network, db and type are required. Provided network: " network " ledger: " ledger-id " type: " type " key: " key)
                                                            {:status 400 :error :db/invalid-request})))
                         auth-id          (cond
 
@@ -794,7 +794,7 @@
 
                                                  {:keys [auth authority]} (http-signatures/verify-request*
                                                                             {:headers headers} :get
-                                                                            (str "/fdb/storage/" network "/" dbid
+                                                                            (str "/fdb/storage/" network "/" ledger-id
                                                                                  (when type (str "/" type))
                                                                                  (when key (str "/" key))) host)
                                                  db          (<? (fdb/db (:conn system) ledger))]
@@ -811,7 +811,8 @@
                                                             (throw (ex-info (str "Auth id for request does not exist in the database: " jwt-auth)
                                                                             {:status 403 :error :db/invalid-auth})))]
                                              jwt-auth))
-                        formatted-key    (cond-> (str network "_" dbid "_" type)
+                        ;; TODO: use storage-core/format-block-key once it's available
+                        formatted-key    (cond-> (str network "_" ledger-id "_" type)
                                            key (str "_" key))
 
                         avro-data        (<? (storage-read-fn formatted-key))
