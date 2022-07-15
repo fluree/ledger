5a87ff515a7b3c0e7b8b722c79e1bd52da611d53
Benjamin Lamothe <bl@732.one>
2022-07-15 17:27:03 +0200

    have a dedicated ns for db->ledger raft transformation

--- src/fluree/db/ledger/upgrade.clj
+++ src/fluree/db/ledger/upgrade.clj
@@ -10,7 +10,6 @@
             [fluree.db.query.range :as query-range]
             [fluree.db.time-travel :as time-travel]
             [fluree.raft.log :as raft-log]
-            [clojure.java.io :as io]
             [clojure.string :as str])
   (:import java.io.File
            (java.nio.file CopyOption Files Path StandardCopyOption)))
@@ -169,62 +168,6 @@
     compatible."
                     {:status 400
                      :error  :db/invalid-request}))))
-
-(defn raft?
-  [group]
-  (some-> group :raft boolean))
-
-(defn raft-log-directory
-  [group]
-  (-> group :raft :config :log-directory io/file))
-
-(defn db->ledger
-  [l]
-  (if (= :append-entry (get l 2))
-    (update-in l [3 :entry 0] (fn [entry-type]
-                                (case entry-type
-                                  :new-db         :new-ledger
-                                  :initialized-db :initialized-ledger
-                                  entry-type)))
-    l))
-
-(defn write-entry
-  [f l]
-  (let [idx        (first l)
-        entry-type (get raft-log/entry-types' idx)]
-    (case entry-type
-      :current-term (let [term (get l 1)]
-                      (raft-log/write-current-term f term))
-      :voted-for    (let [[_ term _ voted-for] l]
-                      (raft-log/write-voted-for f term voted-for))
-      :snapshot     (let [[_ snap-term _ snap-idx] l]
-                      (raft-log/write-snapshot f snap-idx snap-term))
-      (let [[idx _ _ entry] l]
-        (raft-log/write-new-command f idx entry)))))
-
-(defn move-file
-  [^File source ^File target]
-  (Files/move (.toPath source) (.toPath target)
-              (into-array CopyOption
-                          [(StandardCopyOption/REPLACE_EXISTING)])))
-
-(defn v4->v5
-  [{:keys [group] :as _conn}]
-  (go-try
-   (when (raft? group)
-     (let [log-dir   (raft-log-directory group)
-           log-files (->> log-dir
-                          file-seq
-                          (filter (fn [^File f]
-                                    (.isFile f))))]
-       (doseq [^File f log-files]
-         (let [tmp-file (File/createTempFile (.getName f) ".tmp")]
-           (->> f
-                raft-log/read-log-file
-                (map db->ledger)
-                (map (partial write-entry tmp-file))
-                dorun)
-           (move-file tmp-file f)))))))
 
 (def upgrade-fns
   [v1->v2 v2->v3 v3->v4])
---
+++ src/fluree/db/ledger/upgrade/db_ledger.clj
@@ -0,0 +1,61 @@
+(ns fluree.db.ledger.upgrade.db-ledger
+  (:require [fluree.db.util.log :as log]
+            [fluree.raft.log :as raft-log]
+            [clojure.java.io :as io])
+  (:import java.io.File
+           (java.nio.file CopyOption Files Path StandardCopyOption)))
+
+(defn raft?
+  [group]
+  (some-> group :raft boolean))
+
+(defn raft-log-directory
+  [group]
+  (-> group :raft :config :log-directory io/file))
+
+(defn db->ledger
+  [l]
+  (if (= :append-entry (get l 2))
+    (update-in l [3 :entry 0] (fn [entry-type]
+                                (case entry-type
+                                  :new-db         :new-ledger
+                                  :initialized-db :initialized-ledger
+                                  entry-type)))
+    l))
+
+(defn write-entry
+  [f l]
+  (let [idx        (first l)
+        entry-type (get raft-log/entry-types' idx)]
+    (case entry-type
+      :current-term (let [term (get l 1)]
+                      (raft-log/write-current-term f term))
+      :voted-for    (let [[_ term _ voted-for] l]
+                      (raft-log/write-voted-for f term voted-for))
+      :snapshot     (let [[_ snap-term _ snap-idx] l]
+                      (raft-log/write-snapshot f snap-idx snap-term))
+      (let [[idx _ _ entry] l]
+        (raft-log/write-new-command f idx entry)))))
+
+(defn move-file
+  [^File source ^File target]
+  (Files/move (.toPath source) (.toPath target)
+              (into-array CopyOption
+                          [(StandardCopyOption/REPLACE_EXISTING)])))
+
+(defn rewrite-logs
+  [{:keys [group] :as _conn}]
+  (when (raft? group)
+    (let [log-dir   (raft-log-directory group)
+          log-files (->> log-dir
+                         file-seq
+                         (filter (fn [^File f]
+                                   (.isFile f))))]
+      (doseq [^File f log-files]
+        (let [tmp-file (File/createTempFile (.getName f) ".tmp")]
+          (->> f
+               raft-log/read-log-file
+               (map db->ledger)
+               (map (partial write-entry tmp-file))
+               dorun)
+          (move-file tmp-file f))))))
