8bc898b8da2e46c2b35cc4f5e06b3e8f19bbaffc
Benjamin Lamothe <bl@732.one>
2022-08-29 08:59:04 -0400

    add test for pool capacity

--- test/fluree/db/ledger/consensus/update_state_test.clj
+++ test/fluree/db/ledger/consensus/update_state_test.clj
@@ -6,12 +6,21 @@
   (testing "Capped state pools"
     (let [state     {}
           pool-path [:foo :bar]]
-      (testing "with added data"
+      (testing "when empty"
+        (let [k :baz
+              v :bip]
+          (is (not (update-state/in-pool? state pool-path k v))
+              "does not contain any data")))
+      (testing "after adding data"
         (let [k         :baz
-              k'        :nope
               v         :bip
               new-state (update-state/put-pool state 10 pool-path k v)]
           (is (update-state/in-pool? new-state pool-path k v)
-              "contains the added data")
-          (is (not (update-state/in-pool? new-state pool-path k' v))
-                  "does not contain additional data"))))))
+              "contains that data")
+          (testing "when at capacity"
+            (let [limit 1
+                  k'          :bop
+                  v'          :bap
+                  newer-state (update-state/put-pool new-state limit pool-path k' v')]
+              (is (not (update-state/in-pool? newer-state pool-path k' v'))
+                  "does not add more data"))))))))
